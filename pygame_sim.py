e39267baa7677448d842c042d192b4c594ee968e
DawBash <dawood_basharat@hotmail.co.uk>
2022-07-11 20:40:10 +0100

    Added pseudo-sensor system, so every sensed item is sensed at the same range

--- pygame_sim.py
+++ pygame_sim.py
@@ -22,7 +22,7 @@ class Agent(pygame.sprite.Sprite):
         width,
         height,
         color = None,
-        radius = 5,
+        radius = 4.5,
         velocity = [0,0],
     ):
         super().__init__()
@@ -50,24 +50,23 @@ class Agent(pygame.sprite.Sprite):
         self.norm_BRN_list = []
         self.energy_list = []
         self.x_val = 0
-        self.current_BRN_val = random.randint(0,100)
-        print(self.current_BRN_val)
-        self.m = random_floats(-9.99, 9.99, 2)
-        self.c = random_floats(-9.99, 9.99, 2)
+        self.current_BRN_val = 0 #random.randint(0,100)
+        #print(self.current_BRN_val)
+        self.m = 0#+random_floats(-9.99, 9.99, 2)
+        self.c = 0#random_floats(-9.99, 9.99, 2)
         self.energy = 500
         self.in_danger = False
         self.eaten = False
+        self.collision = False
     
     def closest_agent(self):
         '''detects if agent is too close'''
-        '''TODO: migrate to general sensor method that I will implement'''
         closest_agent = min([i for i in agent_container if i is not self], key=lambda i: pow(i.rect.x-self.rect.x, 2) + pow(i.rect.y-self.rect.y, 2))
         dist = pygame.math.Vector2(closest_agent.rect.x, closest_agent.rect.y).distance_to((self.rect.x, self.rect.y))
         return dist
 
     def is_food(self):
         '''detects if food is close enough'''
-        '''TODO: migrate to general sensor method that I will implement'''
         closest_food = min([i for i in food_container], key=lambda i: pow(i.rect.x-self.rect.x, 2)+ pow(i.rect.y-self.rect.y, 2))
         dist = pygame.math.Vector2(closest_food.rect.x, closest_food.rect.y).distance_to((self.rect.x, self.rect.y))
         closest_food_coord = closest_food.rect.x, closest_food.rect.y
@@ -75,11 +74,20 @@ class Agent(pygame.sprite.Sprite):
 
     def is_hazard(self):
         '''detects if a hazard is close by'''
-        '''TODO: migrate to general sensor method that I will implement'''
         closest_hazard = min ([i for i in hazardous_region_container], key=lambda i : pow(i.rect.x-self.rect.x,2) + pow(i.rect.y -self.rect.y,2))
         dist = pygame.math.Vector2(closest_hazard.rect.x, closest_hazard.rect.y).distance_to((self.rect.x, self.rect.y))
         closest_hazard_coord = closest_hazard.rect.x, closest_hazard.rect.y
         return dist
+    
+    def collide_hazard(self):
+            if pygame.sprite.spritecollideany(self, hazardous_region_container):
+                self.collision = True 
+
+    
+
+
+    
+            
 
     def is_deactivated(self):
         '''detects if energy is below threshold and removes agent from simulation (and sprite group) '''
@@ -111,54 +119,59 @@ class Agent(pygame.sprite.Sprite):
         
 
 
-        self.pos += self.vel 
-
+        
+        self.vel += np.random.rand(2) * 2 - 1
         x, y = self.pos
         '''keeps agents within the 'walls' of the simulation'''
         if x < 0:
             self.vel[0] = -self.vel[0]
+            avoiding = True
         
-        if x > self.WIDTH-10:
+        if x > self.WIDTH-25:
             self.vel[0] = -self.vel[0]
+            avoiding = True
+
 
         if y < 0:
             self.vel[1] = -self.vel[1]
+            avoiding = True
 
-        if y > self.HEIGHT-10:
+
+        if y > self.HEIGHT-25:
             self.vel[1] = -self.vel[1]
+            avoiding = True
 
-        self.rect.x = x
-        self.rect.y = y
-        '''will be edited once sensor is added, the detection distances should all be the same and not alter
-            depending on what is being sensed. This is slightly tricky using L2 norm due to the coords of the regions 
-            being defined by the leftmost corner (by pygame), so for now I used 70 as that covers all of the square (it's
-            basically the length of the hypotenuse of the two right-angled triangles which make up the square)'''
 
-        '''checks probability, first line ensures that agents can leave region once they have entered'''
-    
-        if self.is_hazard() < 70:
+       
             
+            
+        for i,j in HAZARDOUS_REGIONS_XY:
             if not self.in_danger:
-                if self.current_BRN_val >= probability():
+                if self.current_BRN_val < probability() and ((i-25<= x <= i+75) and (j-25 <= y <= j+75)):
+                    self.vel[0] = -self.vel[0]
+                    avoiding = True
+                    print('AVOIDING')
+            
+                elif self.current_BRN_val >= probability():
                     pass
             
             elif self.in_danger:
                 pass
-            else:
-                self.vel = -self.vel
-                avoiding = True
-       
-        '''moves away from closest agent should they reach the distance threshold- will alter once the actual sensor method is added'''
+            
+            
+
+        self.rect.x = x
+        self.rect.y = y
+        
+        '''moves away from closest agent should they reach the distance threshold'''
         if len(agent_container) >1:
-            if self.closest_agent() < 15:
+            if self.closest_agent() < 25:
                 self.vel = -self.vel
                 avoiding = True
-        '''makes sure that the agent isn't avoiding anything and then goes to the closest food item within range,
-        this method needs improvement once the sensor is added (and it may be better for the agent to follow a vector towards it rather 
-        than 'jumping' to the coords'''
+        '''makes sure that the agent isn't avoiding anything and then goes to the closest food item within range '''
         if avoiding == False: 
             closest_food = self.is_food()
-            if closest_food[0] < 15:
+            if closest_food[0] < 25:
           
       
                 self.rect.x = closest_food[1][0]
@@ -172,7 +185,6 @@ class Agent(pygame.sprite.Sprite):
         if vel_norm > 5:
             self.vel /= vel_norm
         '''alters the velocity to enable to random walk behaviour '''
-        self.vel += np.random.rand(2) * 2 - 1
         if not self.eaten:
             self.energy -= 1
             self.food_list.append(0)
@@ -185,21 +197,23 @@ class Agent(pygame.sprite.Sprite):
         '''BRN method and updating of food and energy lists'''
         self.energy_list.append(self.energy)
         '''burn in period and max num of iterations '''
-        if len(self.food_list) >= 20 and self.steps_taken < T:
-            '''calculate moving average list'''
-            self.moving_average_list = moving_average(self.food_list,20)
-            '''normalised x value (via the time window) at current time step'''
-            self.x_val = (self.moving_average_list[-1]/20)
-            '''append boldness value at each timestep to list'''
-            self.BRN_list.append(BRN(self.m, self.x_val, self.c))
-            '''normalise the boldness value at current timestep'''
-            self.current_BRN_val = expit(self.BRN_list[-1])
-            '''append to normalised BRN list'''
-            self.norm_BRN_list.append(self.current_BRN_val)
-            print(self.moving_average_list)
+        #if len(self.food_list) >= 20 and self.steps_taken < T:
+        '''calculate moving average list'''
+        #    self.moving_average_list = moving_average(self.food_list,20)
+        '''normalised x value (via the time window) at current time step'''
+       #     self.x_val = (self.moving_average_list[-1]/20)
+        '''append boldness value at each timestep to list'''
+       #     self.BRN_list.append(BRN(self.m, self.x_val, self.c))
+        '''normalise the boldness value at current timestep'''
+       #     self.current_BRN_val = expit(self.BRN_list[-1])
+        '''append to normalised BRN list'''
+        #    self.norm_BRN_list.append(self.current_BRN_val)
+        #    print(self.moving_average_list)
         
-        self.steps_taken += 1
-   
+      #  self.steps_taken += 1
+        #print(self.current_BRN_val)
+        self.pos += self.vel 
+
 
 
 class Food(pygame.sprite.Sprite):
@@ -210,11 +224,12 @@ class Food(pygame.sprite.Sprite):
         width,
         height,
         color = GREEN,
-        radius = 4.5,
+        radius = 4,
     ):
         super().__init__()
         self.image = pygame.Surface(
             [radius * 2, radius * 2])
+        self.circle = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
         self.image.fill(BACKGROUND)
         pygame.draw.circle(
             self.image, color, (radius, radius), radius
@@ -265,7 +280,7 @@ class Hazardous_Regions(pygame.sprite.Sprite):
         self.rect.y = y
 
 def probability():
-    prob = random.randint(0,100)
+    prob = random.randint(1,100)
     return prob
 
 def moving_average(sensory_list, w):
@@ -280,8 +295,8 @@ def random_floats(lower, upper, decimal_places):
     return value
 
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 1000
+HEIGHT = 1000
 pygame.init()
 screen = pygame.display.set_mode(
     [WIDTH, HEIGHT]
@@ -298,7 +313,7 @@ for i in range(10):
 
 food_container = pygame.sprite.Group()
 
-for i in range(200):
+for i in range(100):
     x = np.random.randint(0,WIDTH+1)
     y = np.random.randint(0, HEIGHT+1)
     food = Food(x,y,WIDTH, HEIGHT)
